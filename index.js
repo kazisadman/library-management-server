b03f3538dc093d2fbf83c262bdb1cd905d9dcd34
teethw <kazisadman01@gmail.com>
2023-11-07 14:00:51 +0600

    created post and get method for borrowed books

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oiiyhkk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +30,41 @@ async function run() {
     );
 
     const bookCollection = client.db("booksinfodb").collection("booksinfo");
+    const borrowedCollection = client
+      .db("booksinfodb")
+      .collection("borrowbook");
 
     app.post("/booksinfo", async (req, res) => {
       const newbook = req.body;
       const result = await bookCollection.insertOne(newbook);
       res.send(result);
     });
+
+    app.get("/booksinfo", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/booksinfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/borrowbook", async (req, res) => {
+      const newbook = req.body;
+      const result = await borrowedCollection.insertOne(newbook);
+      res.send(result);
+    });
+
+    app.get("/borrowbook", async (req, res) => {
+      const cursor = borrowedCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
